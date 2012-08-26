4d5dbe4add04475265d476b10074bef9d7e1f992
Artem <artem.cpt@gmail.com>
2012-08-26 23:32:02 +0400

    Окончательно допилен движок воспроизведения

--- public/js/common/stream_player.js
+++ public/js/common/stream_player.js
@@ -1,8 +1,20 @@
 
 $().ready(function () {
+	/* Initialize soundManager */
 	soundManager.flashVersion = 9;
     soundManager.debugMode = false;
+	
+	/*
+	StreamPlayer.setCallbacks({
+		on_song_changed: function(song) {console.log(song);},
+		on_position_changed: function(percent) { console.log(percent); }
+	});
+	*/
 });
+
+function aid2id(aid) {
+	return "a" + aid;
+}
 
 var QueuedPlaybackLoader = (function() {
 
@@ -13,34 +25,38 @@ var QueuedPlaybackLoader = (function() {
 		
 		if (currently_loading_sound) {
 			throw "Assertion failed: currently_loading_sound must be undefined";
+			return;
 		}
 		
 		var song = songs_queue.shift();
 
-		if (song == undefined) {
-			// No songs in queue, return
+		if (song === undefined) {
+			// No songs to load
 			return;
 		}
 		
-		if (soundManager.getSoundById("a" + song.aid) !== undefined) {
-			// If sound was loaded already
-			console.log("Sound already loaded");
+		if (soundManager.getSoundById(aid2id(song.aid)) !== undefined) {
+			// Sound was loaded already
 			loadNextSong();
+			return;
 		}
 		
 		currently_loading_sound = soundManager.createSound({
-			id: "a" + song.aid,
+			id: aid2id(song.aid),
 			url: song.url,
 			volume: 100,
 			autoLoad: true,
 			stream: true,
 			autoPlay: false,
 			onload: function() {
-				console.log("onload() called");
+				//console.log("loading finished");
 				currently_loading_sound = undefined;
 				loadNextSong();
 			}
 		});
+		
+		// Save song object to sound
+		currently_loading_sound.song = song;
 	}
 	
 	return {
@@ -51,17 +67,24 @@ var QueuedPlaybackLoader = (function() {
 			playback.songs.forEach(function(song) {
 				songs_queue.push(song);
 			});
-			
-			if (currently_loading_sound) {
-				
+
+			if (currently_loading_sound !== undefined) {
+				// Some sound is loading right now
+
 				var next_song = songs_queue[0];
-				if (next_song !== undefined && next_song.aid == currently_loading_sound.id) {
-					// Next song is already loading. Ok, do nothing
-					return;
-				} else {
-					// Cansel current song loading
-					currently_loading_sound.destruct();
-					currently_loading_sound = undefined;	
+				if (next_song !== undefined) {
+					if (aid2id(next_song.aid) == currently_loading_sound.id) {
+						// Next song is loading at now. Ok, do nothing
+						return;
+					} else if (soundManager.getSoundById(aid2id(next_song.aid)) !== undefined) {
+						// Next song was loaded already. Ok, do nothing
+						return;
+					} else {
+						// First song in queue has bigger priority, cansel current song loading
+						console.log("Canseling loading");
+						currently_loading_sound.destruct();
+						currently_loading_sound = undefined;
+					}
 				}
 			}
 			
@@ -72,10 +95,16 @@ var QueuedPlaybackLoader = (function() {
 
 var PlaybackPlayer = (function() {
 
-	var songs_queue = [];
-	var currently_playing_sound;
+	var songs_queue = [],
+		currently_playing_sound;
+	
+	var callbacks = {
+		on_song_changed: function() {},
+		on_position_changed: function() {}
+	}
 		
 	function stopPlaying() {
+		console.log("stopPlaying()");
 		if (currently_playing_sound) {
 			currently_playing_sound.stop();
 			currently_playing_sound = undefined;
@@ -93,43 +122,38 @@ var PlaybackPlayer = (function() {
 		});
 		
 		if (current_song == undefined) {
-			console.log("No songs in queue, stop playing");
-			// No songs in queue, return
+			console.log("No songs to play");
 			stopPlaying();
 			return;
 		}
 		
-		var sound = soundManager.getSoundById("a" + current_song.aid);
+		var sound = soundManager.getSoundById(aid2id(current_song.aid));
 		if (sound == undefined) {
-			console.log("Failed to get sound, waiting for second...");
+			console.log("Sound not found, waiting for second...");
 			setTimeout(updatePlaying, 1000);
 			return;
 		}
-		
-		console.log("currently_playing_sound: ", currently_playing_sound);
-		
+				
 		var song_position_offset = now - current_song.start_time;
 		
-		if (currently_playing_sound != undefined && currently_playing_sound.id == sound.id) {
-			// Same sound playing
+		if (currently_playing_sound !== undefined && currently_playing_sound.id == sound.id) {
+			// Same sound is playing
 			var offsetError = Math.abs(currently_playing_sound.position - song_position_offset);
-			
-			console.log("Offset error: ", offsetError);
-			console.log(currently_playing_sound.position, song_position_offset);
-			
+						
 			if (offsetError < 1000) {
 				// Don't need to fix anything
+				//console.log("Already playing: ", offsetError);
 				return;
 			} else {
-				// OffsetError is to large, start again to fix it
+				// OffsetError is to large, restart playing
+				//console.log("Reset sound: ", offsetError);
 			}
-		}			
+		}		
 			
 		stopPlaying();
 						
 		switch (sound.readyState) {
-		case 1: // loading
-			//console.log("Loading: ", sound.duration, song_position_offset + 5000);
+		case 1: // Loading
 			if (sound.duration < song_position_offset + 5000) {
 				console.log("Sound is still loading, waiting for second...");
 				setTimeout(updatePlaying, 1000);
@@ -137,30 +161,40 @@ var PlaybackPlayer = (function() {
 			}
 			// Go ahead to case 3
 					
-		case 3: // loaded
+		case 3: // Loaded
 			console.log("Start playing song at: ", song_position_offset);
 			sound.setPosition(song_position_offset);
 			sound.play({
 				onfinish: function() {
-					console.log("onstop() called");
 					updatePlaying();
+				},
+				
+				whileplaying: function() {
+					var position = currently_playing_sound.position,
+						duration = currently_playing_sound.song.duration,
+						percent = position / (duration * 1000.0);
+										
+					callbacks.on_position_changed(percent);
 				}
 			});
 					
-			currently_playing_sound = sound;		
+			currently_playing_sound = sound;
+			
+			// Notify observers about song change
+			callbacks.on_song_changed(currently_playing_sound.song);
 			break;
 						
 		case 2: // failed error
 		case 0: // uninitialized
 		default:
-			console.log("Failed to get sound, waiting for 5 seconds...");
+			console.log("Sound initialization failed, waiting for 5 seconds...");
 			setTimeout(updatePlaying, 5000);
 			return;
-		}					
+		}
 	}
 			
 	return {
-		playPlayback: function(playback) {
+		play: function(playback) {
 			songs_queue = [];
 			playback.songs.forEach(function(song) {
 				songs_queue.push(song);
@@ -169,21 +203,40 @@ var PlaybackPlayer = (function() {
 			updatePlaying();
 		},
 		
-		stopPlaying: stopPlaying
+		stop: stopPlaying,
+				
+		isPlaying: function() {
+			return currently_playing_sound !== undefined;
+		},
+		
+		getPlayingSong: function() {
+			if (!this.isPlaying()) {
+				return undefined;
+			}			
+			return currently_playing_sound.song;
+		},
+		
+		setCallbacks: function(cb) {
+			callbacks = {
+				on_song_changed: cb.on_song_changed,
+				on_position_changed: cb.on_position_changed
+			}
+		}
 	};
 })();
 
 var StreamPlayer = (function() {
 	
+	var playing_stream_id,
+		is_stream_update_scheduled = false;
+	
+	/* Syncronize by client-server time offset */
 	function fixPlaybackTime(playback, server_time, local_time) {
 		var time_offset = local_time - server_time;
 		playback.songs.forEach(function(song) {
 			song.start_time += time_offset;
 			song.stop_time += time_offset;
 		});
-		
-		console.log("Server time: " + server_time);
-		console.log("Local time: " + local_time);
 	}
 	
     return {
@@ -198,19 +251,53 @@ var StreamPlayer = (function() {
 				if (playback) {
 
 					var local_time = (before_request_time + after_request_time) / 2;
-					fixPlaybackTime(playback, playback.server_time, local_time);			
+					fixPlaybackTime(playback, playback.server_time, local_time);
 					
 					QueuedPlaybackLoader.loadPlayback(playback);
-					PlaybackPlayer.playPlayback(playback);
+					PlaybackPlayer.play(playback);
+					
+					playing_stream_id = stream_id;
+					
 				} else {
 					console.log("No playback received");
-					this.stopPlaying();
+					StreamPlayer.stopPlaying();
+					
+					playing_stream_id = undefined;
+				}
+				
+				if (playing_stream_id !== undefined && !is_stream_update_scheduled) {
+					setTimeout(StreamPlayer.updateStream, 60000); // Update once a minute
+					is_stream_update_scheduled = true;			
 				}
 			});
 		},
-				
+		
+		updateStream: function() {
+			is_stream_update_scheduled = false;
+			if (playing_stream_id !== undefined) {
+				StreamPlayer.playStream(playing_stream_id);
+			}
+		},
+		
 		stopPlaying: function() {
-			PlaybackPlayer.stopPlaying();
+			playing_stream_id = undefined;
+			PlaybackPlayer.stop();
+		},
+		
+		getStreamID: function() {
+			return playing_stream_id;
+		},
+		
+		isPlaying: function() {
+			return PlaybackPlayer.isPlaying();
+		},
+		
+		getPlayingSong: function() {
+			return PlaybackPlayer.getPlayingSong();
+		},
+		
+		setCallbacks: function(callbacks) {
+			PlaybackPlayer.setCallbacks(callbacks);
 		}
     };
 })();
